4876e8faebeac597b5e7aa14af26c1902a7f07d3
li-yp <yunpeng.li@surrey.ac.uk>
2025-01-05 13:54:16 +0000

    Deploying to gh-pages from @ li-yp/li-yp.github.io@b82db0138421942d758f1ec777c10b632e4826c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,48 +9,71 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
-    },{id: "news-we-started-the-humbug-ii-project-in-collaboration-with-the-amazing-oxford-team-led-by-kathy-willis-and-steve-roberts-the-project-is-generously-sponsored-by-the-natural-environment-research-council-in-its-innovation-in-environmental-monitoring-programme",
-          title: 'We started the HumBug II project in collaboration with the amazing Oxford team...',
+  },{id: "nav-team",
+          title: "Team",
           description: "",
-          section: "News",},{id: "news-zhi-qin-tan-presented-bayesian-detector-combination-for-object-detection-with-crowdsourced-annotations-at-eccv-2024-in-milan-italy-as-his-first-phd-paper-in-phd-the-work-is-in-collaboration-with-olga-isupova-gustavo-carneiro-and-xiatian-zhu",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/team/";
+          },
+        },{id: "nav-openings",
+          title: "Openings",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/opening/";
+          },
+        },{id: "post-a-post-with-image-galleries",
+      
+        title: "a post with image galleries",
+      
+      description: "this is what included image galleries could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/photo-gallery/";
+        
+      },
+    },{id: "post-a-post-with-tabs",
+      
+        title: "a post with tabs",
+      
+      description: "this is what included tabs in a post could look like",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/tabs/";
+        
+      },
+    },{id: "news-we-started-the-humbug-ii-project-in-collaboration-with-the-oxford-team-led-by-kathy-willis-and-steve-roberts-the-project-is-sponsored-by-the-natural-environment-research-council-in-its-innovation-in-environmental-monitoring-programme",
+          title: 'We started the HumBug II project in collaboration with the Oxford team led...',
+          description: "",
+          section: "News",},{id: "news-zhi-qin-tan-presented-bayesian-detector-combination-for-object-detection-with-crowdsourced-annotations-at-eccv-2024-in-milan-italy-as-his-first-paper-in-phd-the-work-is-in-collaboration-with-olga-isupova-gustavo-carneiro-and-xiatian-zhu",
           title: 'Zhi Qin Tan presented Bayesian Detector Combination for Object Detection with Crowdsourced Annotations...',
           description: "",
           section: "News",},{id: "news-dr-xiongjie-chen-zhi-qin-tan-and-john-joseph-brady-completed-their-transfer-from-the-university-of-surrey-to-king-s-college-london-as-inaugural-members-of-the-statistical-machine-learning-lab-based-in-the-centre-for-oral-clinical-amp-amp-translational-sciences",
           title: 'Dr Xiongjie Chen, Zhi Qin Tan, and John-Joseph Brady completed their transfer from...',
           description: "",
+          section: "News",},{id: "news-owen-addison-and-i-published-an-opinion-piece-at-bdj-in-practice-to-discuss-our-nihr-sponsored-project-and-becertain-ai",
+          title: 'Owen Addison and I published an opinion piece at BDJ in Practice to...',
+          description: "",
           section: "News",},{id: "news-i-introduced-our-internal-testing-product-of-radiograph-based-dental-disease-detection-accessible-via-becertain-ai-at-royal-college-of-surgeons-of-england-in-the-workshop-of-ai-and-digital-innovation-in-dentistry-and-oral-health-revolutionising-approaches-to-care-or-just-another-myth",
           title: 'I introduced our internal testing product of radiograph-based dental disease detection, accessible via...',
+          description: "",
+          section: "News",},{id: "news-i-gave-a-talk-on-normalizing-flow-based-differentiable-particle-filters-at-the-kcl-statistics-seminar",
+          title: 'I gave a talk on Normalizing Flow-based Differentiable Particle Filters at the KCL...',
           description: "",
           section: "News",},{id: "news-i-was-selected-into-the-royal-academy-of-engineering-explore-4-0-cohort-to-explore-new-ecosystems-for-our-work-on-ai-assisted-dental-disease-detection",
           title: 'I was selected into the Royal Academy of Engineering Explore 4.0 cohort to...',
           description: "",
-          section: "News",},{id: "news-we-are-recruiting-a-full-phd-studentship-is-provided-in-the-broad-areas-of-multimodal-ai-and-digital-healthcare",
-          title: 'We are recruiting! A full PhD studentship is provided in the broad areas...',
+          section: "News",},{id: "news-we-are-recruiting-a-fully-funded-phd-position-is-available-in-the-broad-areas-of-multimodal-ai-and-digital-healthcare",
+          title: 'We are recruiting! A fully-funded PhD position is available in the broad areas...',
           description: "",
           section: "News",},{id: "news-our-paper-normalizing-flow-based-differentiable-particle-filters-has-been-accepted-at-ieee-transactions-on-signal-processing-congratulations-xiongjie",
           title: 'Our paper Normalizing Flow-based Differentiable Particle Filters has been accepted at IEEE Transactions...',
+          description: "",
+          section: "News",},{id: "news-we-welcome-phd-applicants-to-apply-for-the-epsrc-centre-for-doctoral-training-in-data-driven-health-drive-health-programme-for-our-project-multimodal-dental-ai-with-uncertainty-quantification-the-application-deadline-is-30th-january-2025-see-openings-for-available-phd-positions-in-the-group",
+          title: 'We welcome PhD applicants to apply for the EPSRC Centre for Doctoral Training...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -97,6 +120,21 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-john-joseph-brady",
+          title: 'John-Joseph Brady',
+          description: "PhD student (2023-)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/People_John_Joseph_Brady/";
+            },},{id: "projects-xiongjie-chen",
+          title: 'Xiongjie Chen',
+          description: "Research Fellow in Machine Learning in Medical Imaging (2023-). PhD (2019-2023)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/People_Xiongjie_Chen/";
+            },},{id: "projects-zhi-qin-tan",
+          title: 'Zhi Qin Tan',
+          description: "PhD student (2023-)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/People_Zhi_Qin_Tan/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
